110d9e403eea549fb7d0d7c8efd2b22e57208261
NikolaiEmelianov <nikolayemelyanov7@gmail.com>
2020-09-08 11:10:04 +0300

    Initial commit

---
+++ src/test/java/com/servicehub/WeatherServiceAdapter/WeatherServiceAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicehub.WeatherServiceAdapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServiceAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
